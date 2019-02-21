98a731e9c491825e7444447cc122a19150cbdd26
netjoin <netjoin@gmail.com>
2019-02-21 08:51:13 +0800

    批量设置函数别名

--- src/functions.php
+++ src/functions.php
@@ -1,2 +1,39 @@
 <?php
 
+function func($variable = [], $classes = [])
+{
+    if (is_string($variable)) {
+        $arr = func_get_args();
+        array_shift($arr);
+        $variable = [$variable => $arr];
+        $classes = [];
+    }
+
+    foreach ($classes as $key => $value) {
+        $nm = "\Func\\$value";
+        $obj = new $nm;
+    }
+
+    foreach ($variable as $key => $value) {
+        $origin = $key;
+        $name = array_shift($value);        
+        $len = count($value);
+        $str = '';
+        $pieces = [];
+        for ($i = 0; $i < $len; $i++) {
+            $s = "\$value[$i]";
+            $pieces[] = $s;
+        }
+        $arg = implode(', ', $pieces);
+
+        if (is_object($name)) {
+            $func = $name;
+            $origin = "\$func";
+            $name = $key;
+        } else {
+            $origin = "'$origin'";
+        }
+        $code = "new \Func\Alias($origin, '$name', $arg);";
+        eval($code);
+    }
+}
