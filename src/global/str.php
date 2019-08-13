caf00dc69ef48b1e724ed7ecbc3f2ff17d9e334b
wuding <netjoin@gmail.com>
2019-08-13 20:38:56 +0800

    添加全局调用 unicode_encode，strtoarray，unicode_convert

--- src/global/str.php
+++ src/global/str.php
@@ -6,5 +6,20 @@ new \Func\Str;
 
 function unicode_decode($str, $type = null)
 {
-	return f\unicode_decode($str, $type);
+    return f\unicode_decode($str, $type);
+}
+
+function unicode_encode($str)
+{
+    return f\unicode_encode($str);
+}
+
+function strtoarray($str, $columns = 6)
+{
+    return f\strtoarray($str, $columns);
+}
+
+function unicode_convert($string)
+{
+    return f\unicode_convert($string);
 }
