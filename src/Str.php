b2ba92276fb203a6b4f0578fec3846bac6e737a4
wuding <netjoin@gmail.com>
2019-08-13 19:10:30 +0800

    添加 unicode 编码，修改 unicode 解码

--- src/Str.php
+++ src/Str.php
@@ -4,28 +4,40 @@ namespace Func;
 
 class Str
 {
-	public function __construct()
-	{
+    public function __construct()
+    {
 
-	}
+    }
 }
 
 /**
  * Unicode 解码
  * 
  * @param  string $str    要解码的字符串
- * @param  bool   $type   ？
+ * @param  bool   $type   是否 JSON 格式
  * 
  * @return array         返回解码结果
  */
-function unicode_decode($str, $type = null)
-{
-    $str = trim($str);
-    if (!$type || !$str) {
-        return $str;
+function unicode_decode($str, $type = null) {
+    $json = true === $type ? $str : '{"str":"' . $str . '"}';
+    $obj = json_decode($json);
+    if (!$obj) {
+        return '';
     }
+    return $obj->str;
+}
 
-    $str = preg_replace('/\"&#13;/', '', $str);
-    $obj = json_decode('{"str":"' . $str . '"}');
-    return _isset($obj, 'str', '');
+
+/**
+ * Unicode 编码
+ *
+ * @param      string  $str    要编码的字符串
+ *
+ * @return     string  返回编码结果
+ */
+function unicode_encode($str)
+{
+    $arr = array('str' => $str);
+    $json = json_encode($arr);
+    return $result = preg_replace('/^{"str":"|"}$/', '', $json);
 }
